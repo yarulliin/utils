b849d2a85191cfbd394fe8471185f404d7cc84fe
Rifat Yarullin <rifat.jarullin@gmail.com>
2021-02-23 13:55:20 +0300

    union func has been changed

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,26 +1,26 @@
 function union(a, b) {
     if (Array.isArray(a) && Array.isArray(b)) {
-        return [...new Set([...a, ...b])];
+        return [...a, ...b];
     }
 }
 
 function intersection(a, b) {
     if (Array.isArray(a) && Array.isArray(b)) {
-        let res = a.filter(el => b.includes(el));
+        const res = a.filter(el => b.includes(el));
         return res;
     }
 }
 
 function difference(a, b) {
     if (Array.isArray(a) && Array.isArray(b)) {
-        let res = a.filter(el => !b.includes(el));
+        const res = a.filter(el => !b.includes(el));
         return res;
     }
 }
 
 function repeat(arr, count) {
     if (Array.isArray(a)) {
-        let res = [];
+        const res = [];
         arr.forEach(el => {
             for (let i = 0; i < count; i++) {
                 res.push(el)
