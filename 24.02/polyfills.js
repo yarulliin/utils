152699eb1a9fdc910704dc5854e0f94f517b4652
Rifat Yarullin <rifat.jarullin@gmail.com>
2021-02-26 10:50:28 +0300

    add Object.create poly

--- 24.02/polyfills.js
+++ 24.02/polyfills.js
@@ -3,3 +3,13 @@ function bindPoly(func, context) {
         return func.apply(context, args);
     }
 }
+
+function objCreate(proto) {
+    function Text() {
+        obj = {};
+        obj.__proto__ = proto;
+        return obj;
+    }
+
+    return new Text();
+}
